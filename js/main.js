d4502dd3a04d5ae42ba619b75ee5fc211b903c59
Alexandra <alexandra.bazanova@gmail.com>
2021-02-05 15:50:28 +0300

    исправления по заданию Больше деталей

--- js/main.js
+++ js/main.js
@@ -32,14 +32,13 @@ return -1;
 //****************************************************************************
 const SIMILAR_ADVERT_COUNT = 10;
 const TITLES = ['Best offer', 'Great location', 'Choice of visitors', 'Top 100', 'Eco-friendly', 'Summer offer', 'For family holidays', 'Best city view'];
-const TYPE_OF_APPARTMENT = ['palace', 'flat', 'house', 'bungalow'];
-const ROOM_COUNTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const GUEST_COUNTS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-const TIME_CHECKIN = ['12:00', '13:00', '14:00'];
-const TIME_CHECKOUT = ['12:00', '13:00', '14:00'];
+const TYPES_OF_APPARTMENTS = ['palace', 'flat', 'house', 'bungalow'];
+const ROOMS_COUNTS = [1, 2, 3, 100];
+const GUESTS_COUNTS = [1, 2, 3, 4];
+const TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const APPARTMENT_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-const DESCRIPTION = ['Do not miss the best deal!', 'All sights of the city within walking distance', 'Helpful staff, comfortable location, parking on request', 'Services of a guide-translator, nanny, organization of weddings and celebrations. To get the service, contact the reception', 'Thank you Google translate'];
+const APPARTMENTS_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const DESCRIPTIONS = ['Do not miss the best deal!', 'All sights of the city within walking distance', 'Helpful staff, comfortable location, parking on request', 'Services of a guide-translator, nanny, organization of weddings and celebrations. To get the service, contact the reception', 'Thank you Google translate'];
 
 const getOneRandomItem = (arr) => arr[getRandomNumber(0, arr.length - 1)];
 
@@ -52,7 +51,8 @@ const createAdvert = () => {
   const getPoint = {
     x: getRandomArbitrary(35.65000, 35.70000, 5),
     y:getRandomArbitrary(139.70000, 139.80000, 5)
-  }
+  };
+  const adTime = getOneRandomItem(TIMES);
   return {
     author: {
       avatar: `img/avatars/user0${getRandomNumber(1, 8)}.png`
@@ -60,15 +60,15 @@ const createAdvert = () => {
     offer: {
       title: getOneRandomItem(TITLES),
       address: `location.${getPoint.x}, location.${getPoint.y}`,
-      price: getRandomNumber(1000, 100000),
-      type: getOneRandomItem(TYPE_OF_APPARTMENT),
-      rooms: getOneRandomItem(ROOM_COUNTS),
-      guests: getOneRandomItem(GUEST_COUNTS),
-      checkin: getOneRandomItem(TIME_CHECKIN),
-      checkout: getOneRandomItem(TIME_CHECKOUT),
+      price: getRandomNumber(0, 1000000),
+      type: getOneRandomItem(TYPES_OF_APPARTMENTS),
+      rooms: getOneRandomItem(ROOMS_COUNTS),
+      guests: getOneRandomItem(GUESTS_COUNTS),
+      checkin: adTime,
+      checkout: adTime,
       features: getRandomItemsArray(FEATURES, getRandomNumber(0, FEATURES.length - 1)),
-      description: getOneRandomItem(DESCRIPTION),
-      photos: getRandomItemsArray(APPARTMENT_PHOTOS, getRandomNumber(0, APPARTMENT_PHOTOS.length - 1)),
+      description: getOneRandomItem(DESCRIPTIONS),
+      photos: getRandomItemsArray(APPARTMENTS_PHOTOS, getRandomNumber(0, APPARTMENTS_PHOTOS.length - 1)),
     },
     location: {
       x: getPoint.x,
