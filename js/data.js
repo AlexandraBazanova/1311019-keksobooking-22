1cbc074538f0f2d7c71338427069b0a1f8242973
Alexandra <alexandra.bazanova@gmail.com>
2021-02-15 13:35:39 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,14 +1,30 @@
-export {createAdvert};
 import {getRandomNumber, getRandomArbitrary, getOneRandomItem, getRandomItemsArray} from './utils.js';
 
 const TITLES = ['Best offer', 'Great location', 'Choice of visitors', 'Top 100', 'Eco-friendly', 'Summer offer', 'For family holidays', 'Best city view'];
-const TYPES_OF_APPARTMENTS = ['palace', 'flat', 'house', 'bungalow'];
+const TYPES_OF_APPARTMENTS = [{
+  en: 'flat',
+  ru: 'Квартира'
+},
+{
+  en: 'palace',
+  ru: 'Дворец'
+},
+{
+  en: 'house',
+  ru: 'Дом'
+},
+{
+  en: 'bungalow',
+  ru: 'Бунгало'
+}
+];
 const ROOMS_COUNTS = [1, 2, 3, 100];
 const GUESTS_COUNTS = [1, 2, 3, 4];
 const TIMES = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const APPARTMENTS_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 const DESCRIPTIONS = ['Do not miss the best deal!', 'All sights of the city within walking distance', 'Helpful staff, comfortable location, parking on request', 'Services of a guide-translator, nanny, organization of weddings and celebrations. To get the service, contact the reception', 'Thank you Google translate'];
+const SIMILAR_ADVERT_COUNT = 10;
 
 const createAdvert = () => {
   const getPoint = {
@@ -24,14 +40,14 @@ const createAdvert = () => {
       title: getOneRandomItem(TITLES),
       address: `location.${getPoint.x}, location.${getPoint.y}`,
       price: getRandomNumber(0, 1000000),
-      type: getOneRandomItem(TYPES_OF_APPARTMENTS),
+      type: getOneRandomItem(TYPES_OF_APPARTMENTS).ru,
       rooms: getOneRandomItem(ROOMS_COUNTS),
       guests: getOneRandomItem(GUESTS_COUNTS),
       checkin: adTime,
       checkout: adTime,
       features: getRandomItemsArray(FEATURES, getRandomNumber(0, FEATURES.length - 1)),
       description: getOneRandomItem(DESCRIPTIONS),
-      photos: getRandomItemsArray(APPARTMENTS_PHOTOS, getRandomNumber(0, APPARTMENTS_PHOTOS.length - 1)),
+      photos: getRandomItemsArray(APPARTMENTS_PHOTOS, getRandomNumber(0, APPARTMENTS_PHOTOS.length)),
     },
     location: {
       x: getPoint.x,
@@ -39,3 +55,7 @@ const createAdvert = () => {
     }
   };
 };
+
+const similarAdvert = new Array(SIMILAR_ADVERT_COUNT).fill(null).map(() => createAdvert());
+console.log(similarAdvert)
+export {similarAdvert};
---
+++ js/popup.js
@@ -0,0 +1,77 @@
+import {similarAdvert} from './data.js';
+export {renderCard};
+
+
+const renderCard = function (announcement) {
+  console.log(announcement)
+  //Находим фрагмент с содержимым темплейта
+  const templateFragment = document.querySelector('#card').content;
+  //Клонируем фрагмент со всеми внутренностями
+  const cardElement = templateFragment.cloneNode(true);
+  //В фрагменте находим нужные элементы
+  const adsElement = cardElement.querySelector('.popup');
+
+  adsElement.querySelector('.popup__title').textContent = announcement.offer.title;
+  adsElement.querySelector('.popup__text--address').textContent = announcement.offer.address;
+  adsElement.querySelector('.popup__text--price').textContent = `${announcement.offer.price} ₽/ночь`;
+  adsElement.querySelector('.popup__type').textContent = announcement.offer.type;
+  adsElement.querySelector('.popup__text--capacity').textContent = `${announcement.offer.rooms} комнаты для ${announcement.offer.guests} гостей`;
+  adsElement.querySelector('.popup__text--time').textContent = `Заезд после ${announcement.offer.checkin}, выезд до ${announcement.offer.checkout}`;
+
+  const getFeatures = function () {
+    //Создаем контейнер для features
+    const featuresFragment = document.createDocumentFragment();
+    for (let i = 0; i < announcement.offer.features.length; i++) {
+      //Создаем список с актуальными для объявления элементами
+      const newElement = document.createElement('li');
+      //Показываем, как должен называться класс у каждого элемента
+      const newClassFeature = 'popup__feature--' + announcement.offer.features[i];
+      //Добавляем этот класс
+      newElement.classList.add('popup__feature', newClassFeature);
+      //Складываем элементы в контейнер
+      featuresFragment.appendChild(newElement);
+    }
+    return featuresFragment;
+  };
+  const featuresList = adsElement.querySelector('.popup__features');
+  //Очищаем от стандартного (полного) набора features
+  featuresList.innerHTML = '';
+  //Складываем в коробочку с классом "popup__features" только элементы из getFeatures
+  featuresList.appendChild(getFeatures(announcement.offer.features));
+
+  adsElement.querySelector('.popup__description').textContent = announcement.offer.description;
+
+  const getImage = function (arr) {
+    //Ищем элементы с классом popup__photo
+    const image = adsElement.querySelector('.popup__photo');
+      if (arr.length == 1) {
+        image.setAttribute("src", arr[0]);
+      }
+        if (arr.length > 1) {
+          for (let i = 1; i < arr.length ; i++) {
+            image.setAttribute("src", arr[0]);
+            const imageClone = image.cloneNode();
+            adsElement.appendChild(imageClone);
+            imageClone.setAttribute("src", arr[i]);
+          }
+        }
+          else if (arr.length == 0) {
+            image.classList.add('hidden');
+          }
+  };
+  getImage(announcement.offer.photos);
+
+  const cardAvatar = adsElement.querySelector('.popup__avatar');
+  cardAvatar.setAttribute("src", announcement.author.avatar);
+
+  return cardElement;
+};
+//Создаем коробочку для объявлений
+const fragment = document.createDocumentFragment();
+//Складываем в коробочку только первое объявление
+fragment.appendChild(renderCard(similarAdvert[0]));
+
+//Отрисовываем первое объявление
+const advert = document.querySelector('.map__canvas');
+advert.appendChild(fragment);
+
